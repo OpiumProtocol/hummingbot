d1b4781b4bb13b8f510252435dcdbd2a4ee30a0d
Dmytro Safonov <dmytro.safonov@seznam.cz>
2020-08-15 23:24:54 +0200

    Fill basic files for integration

--- conf/__init__.py
+++ conf/__init__.py
@@ -105,6 +105,10 @@ coinalpha_order_book_api_username = "***REMOVED***"
 coinalpha_order_book_api_password = "***REMOVED***"
 """
 
+# TODO: Opium-verify
+opium_api_key = os.getenv("NEW_MARKET_API_KEY")
+opium_secret_key = os.getenv("NEW_MARKET_SECRET_KEY")
+
 kafka_2 = {
     "bootstrap_servers": "***REMOVED***",
     "zookeeper_servers": "***REMOVED***"
