8c131bf133a66bbe60f450a921c5e84e90b02238
KonstantinLitvin <konstantinlitvin89@gmail.com>
2020-10-25 16:51:07 +0300

    added listen_for_user_stream added exception handling

--- hummingbot/market/opium/opium_api_user_stream_data_source.py
+++ hummingbot/market/opium/opium_api_user_stream_data_source.py
@@ -3,7 +3,7 @@ import logging
 from typing import Optional
 
 from opium_api import OpiumClient
-from sockets import OpiumApi
+from opium_sockets import OpiumApi
 
 from hummingbot.core.data_type.user_stream_tracker_data_source import \
     UserStreamTrackerDataSource
@@ -23,8 +23,6 @@ class OpiumAPIUserStreamDataSource(UserStreamTrackerDataSource):
         self._opium_client: OpiumClient = opium_client
         self._opium_socketio: OpiumApi = OpiumApi(test_api=True)
 
-        self._last_recv_time = NotImplemented
-
     @property
     def last_recv_time(self) -> float:
         return self._opium_socketio.get_last_message_time()
@@ -35,11 +33,12 @@ class OpiumAPIUserStreamDataSource(UserStreamTrackerDataSource):
     async def listen_for_user_stream(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
 
         trading_pair = 'OEX-FUT-1DEC-135.00'
+        token = '0x' + self._opium_client.generate_access_token()
         while True:
             try:
                 await self._opium_socketio.listen_for_orders(trading_pair=trading_pair,
                                                              maker_addr=self._opium_client.get_public_key(),
-                                                             sig=self._opium_client.generate_access_token(),
+                                                             sig=token,
                                                              output=output)
 
             except Exception as ex:
