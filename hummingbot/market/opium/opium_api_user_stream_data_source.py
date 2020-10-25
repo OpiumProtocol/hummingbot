70ad3b18163fc5f1d6a618d14d0c9908808afb6a
KonstantinLitvin <konstantinlitvin89@gmail.com>
2020-10-25 12:28:57 +0300

    added listen_for_user_stream added exception handling

--- hummingbot/market/opium/opium_api_user_stream_data_source.py
+++ hummingbot/market/opium/opium_api_user_stream_data_source.py
@@ -1,5 +1,48 @@
-from hummingbot.core.data_type.user_stream_tracker_data_source import UserStreamTrackerDataSource
+import asyncio
+import logging
+from typing import Optional
+
+from opium_api import OpiumClient
+from sockets import OpiumApi
+
+from hummingbot.core.data_type.user_stream_tracker_data_source import \
+    UserStreamTrackerDataSource
+from hummingbot.logger import HummingbotLogger
 
 
 class OpiumAPIUserStreamDataSource(UserStreamTrackerDataSource):
-    pass
+    _logger: Optional[HummingbotLogger] = None
+
+    @classmethod
+    def logger(cls) -> HummingbotLogger:
+        if cls._logger is None:
+            cls._logger = logging.getLogger(__name__)
+        return cls._logger
+
+    def __init__(self, opium_client):
+        self._opium_client: OpiumClient = opium_client
+        self._opium_socketio: OpiumApi = OpiumApi(test_api=True)
+
+        self._last_recv_time = NotImplemented
+
+    @property
+    def last_recv_time(self) -> float:
+        return self._opium_socketio.get_last_message_time()
+
+    async def get_listen_key(self):
+        pass
+
+    async def listen_for_user_stream(self, ev_loop: asyncio.BaseEventLoop, output: asyncio.Queue):
+
+        trading_pair = 'OEX-FUT-1DEC-135.00'
+        while True:
+            try:
+                await self._opium_socketio.listen_for_orders(trading_pair=trading_pair,
+                                                             maker_addr=self._opium_client.get_public_key(),
+                                                             sig=self._opium_client.generate_access_token(),
+                                                             output=output)
+
+            except Exception as ex:
+                self.logger().error(f"{ex} error while maintaining the user event listen key. Retrying after "
+                                    "5 seconds...", exc_info=True)
+                await asyncio.sleep(5)
